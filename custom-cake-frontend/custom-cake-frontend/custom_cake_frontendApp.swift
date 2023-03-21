35d5011c02d0344c1338fa4eee9a1de27ca53cef
swiftie1230 <swiftie1230@naver.com>
2023-03-21 20:19:57 +0900

    created custom-cake-frontend

---
+++ custom-cake-frontend/custom-cake-frontend/custom_cake_frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  custom_cake_frontendApp.swift
+//  custom-cake-frontend
+//
+//  Created by 황서진 on 2023/03/21.
+//
+
+import SwiftUI
+
+@main
+struct custom_cake_frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
