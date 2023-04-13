28741c44fed9ba2550bcc842325f6eb410a39d42
kimyoungi99 <kimyoungi99@naver.com>
2023-04-13 11:28:56 +0900

    feat : added cloumn to store (region)

--- custom-cake-backend/src/main/kotlin/com/cake/customcakebackend/common/TempStoreRegion.kt
+++ custom-cake-backend/src/main/kotlin/com/cake/customcakebackend/common/TempStoreRegion.kt
@@ -1,6 +1,18 @@
 package com.cake.customcakebackend.common
 
+import com.fasterxml.jackson.annotation.JsonCreator
+
 enum class TempStoreRegion(val type: String) {
     SEOUNGBUK("성북구"),
-    SEOUNGDONG("성동구"),
+    SEOUNGDONG("성동구");
+
+
+    companion object {
+        @JsonCreator
+        fun set(type: String): TempStoreRegion? = TempStoreRegion.values().find { it.type == type }
+
+        private val shareTypeMap = TempStoreRegion.values().associateBy(TempStoreRegion::type)
+        fun find(type: String): TempStoreRegion? = shareTypeMap[type]
+        fun toList(): List<String> = TempStoreRegion.values().map { it.type }
+    }
 }
