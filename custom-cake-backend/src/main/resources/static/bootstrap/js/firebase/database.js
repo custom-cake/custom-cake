7e1ea2bfc05bb581fcc8c62de20eb875afa4d162
ChoiJaeEun <poungki0823@gmail.com>
2023-05-02 01:27:33 +0900

    리팩토링

--- custom-cake-backend/src/main/resources/static/bootstrap/js/firebase/database.js
+++ custom-cake-backend/src/main/resources/static/bootstrap/js/firebase/database.js
@@ -1,46 +1,50 @@
 import "https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.13.1/underscore-min.js";
 import {
     ref,
-    get, set,
-    child,
+    get,
+    push, update,
     onValue, query,
-    limitToLast, orderByChild,
+    limitToLast, orderByChild, child,
+    serverTimestamp
 } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-database.js";
 import {database} from "./init.js";
 
 
 // 전역변수
-let isOpenRoom = true;
-const MAKE_CHAR = '@make@';
-const DATETIME_CHAR = '@time@';
-const SPLIT_CHAR = '@spl@';
+let currRoomId = '';
+let currOperatorId = 0;
 
 function clearTextarea() {
     $('div.input-div textarea').val('');
 }
 
-window.init = function() {
+window.init = function(operatorId) {
+    const $divChat = $('div.chat');
+    $divChat.animate({
+        scrollTop : $divChat.scrollHeight
+    }, 100);
 
+    // 채팅 페이지 onload
     // enter 키 이벤트
     $('#input-message').on('keydown', function(e){
         if(e.keyCode === 13 && !e.shiftKey) {
             e.preventDefault();
+            const message = $(this).val();
+            currOperatorId = operatorId;
             // 메시지 전송
-            sendMessage($(this).val());
+            sendMessage(message);
             // 입력창 clear
             clearTextarea();
         }
     });
-}
-window.init();
 
+}
 
 window.getOperatorData = function () {
     // onValue() : 경로의 데이터를 읽고 변경사항을 수신 대기할 수 있음.
     onValue(ref(database, 'Operators'), (snapshot) => {
         if (snapshot.exists()) {
             console.log(snapshot.val()["OPERATOR-1"]);
-            // document.getElementById("operatorObject").innerHTML = snapshot.val()["OPERATOR-1"];
             return snapshot.val()["OPERATOR-1"];
         } else {
             console.log("운영자 데이터 없음");
@@ -67,7 +71,7 @@ window.loadOperatorChatRoomList = function (operatorId, chatStatus) {
 
     onValue(chatRoomRef, (snapshot) => {
         if (snapshot.exists()) {
-            console.log(snapshot.val());
+            console.log("채팅방 리스트",snapshot.val());
             getChatRoomList(snapshot, chatStatus);
         } else {
             console.log("No data available.");
@@ -82,25 +86,25 @@ window.loadOperatorChatRoomList = function (operatorId, chatStatus) {
  */
 window.getChatRoomList = function (snapshot, chatStatus) {
     const chatroomTemplate = document.getElementById('templateChatRoomList').innerHTML;
-    let chatRoomList = '';
+    let arrChatRoomList = [];
     let cbDisplayChatRoomList = function (data) {
         const chatRoomId = data.key;
         const chatRoom = data.val();
         if (chatRoomId !== null && chatRoom.chatStatus === chatStatus) {
-            chatRoomList += _.template(chatroomTemplate)({
+            arrChatRoomList.push(_.template(chatroomTemplate)({
                 targetChatRoomUid: chatRoomId,
                 lastMessage: chatRoom.lastMessage,
                 roomUserName: chatRoom.roomUserName,
                 roomUserID: chatRoom.roomUserID,
                 timestamp: timestampToTime(chatRoom.timestamp)
-            });
+            }));
         }
     }
     snapshot.forEach((data) => {
         cbDisplayChatRoomList(data)
     });
     const ulChatRoomList = document.getElementById('ulChatRoomList');
-    ulChatRoomList.innerHTML = chatRoomList;
+    ulChatRoomList.innerHTML = arrChatRoomList.reverse().join('');
 
     /** 채팅방 click event 적용 */
     let arrLiList = ulChatRoomList.getElementsByTagName('tr');
@@ -118,21 +122,19 @@ window.onChatRoomClick = function (event) {
     const roomUserName = event.currentTarget.getAttribute('data-roomUserName');
     const roomTitle = roomUserName + '님';
 
-    openChatRoom(roomId, roomTitle)
+    openChatRoom(roomId, roomTitle);
 }
 
 /**
  * 채팅방 오픈 + message load
  */
 window.openChatRoom = function (roomId, roomTitle) {
+    currRoomId = roomId;
     const spTitle = document.getElementById('spTitle');
-    // const tabMessageList = document.getElementById('tabMessageList');  // 채팅방 화면 보이기
-    isOpenRoom = true;
     if (roomTitle) {
         spTitle.innerHTML = roomTitle;
     }
     loadMessageList(roomId);  // message load
-    // tabMessageList.click();
 }
 
 /**
@@ -141,30 +143,15 @@ window.openChatRoom = function (roomId, roomTitle) {
 window.loadMessageList = function (roomId) {
     const loadMessage = function (roomId, snapshot) {
         // message 화면 리셋
-        ulMessageList.innerHTML = '';
-        let messageTemplate = document.getElementById('templateMessageList').innerHTML;
+        ulMessageList.innerHTML = ''
         let cbDisplayMessages = function (data) {
-            let messageHtml = '';
             const val = data.val();
-            //
+            // message rendering
             if (val.senderType === 'OPERATOR') {
                 appendMessageTag("right", val.sender, val.message);
             } else {
                 appendMessageTag("left", val.sender, val.message);
             }
-
-            // messageHtml = _.template(messageTemplate)({
-            //     key: data.key,  // messageId
-            //     // profileImg : val.profileImg
-            //     memberUid: val.memberUid,
-            //     sender: val.sender,  // == userName
-            //     senderType: val.senderType,
-            //     message: val.message,
-            //     timestamp: timestampToTime(val.timestamp)
-            // });
-
-            ulMessageList.innerHTML += messageHtml;
-            ulMessageList.scrollTop = ulMessageList.scrollHeight;
         }
         snapshot.forEach((data) => {
             cbDisplayMessages(data)
@@ -175,9 +162,13 @@ window.loadMessageList = function (roomId) {
         const chatLi = createMessageTag(LR_className, senderName, message);
 
         $('div.chat:not(.format) ul').append(chatLi);
-
+        // TODO 메시지 scroll 설정
         // 스크롤바 아래 고정
-        $('div.chat').scrollTop($('div.chat').prop('scrollHeight'));
+        const $divChat = $('div.chat');
+        // $divChat.scrollTop($divChat.prop('scrollHeight'));
+        $divChat.animate({
+            scrollTop : $divChat.scrollHeight
+        }, 100);
     };
 
     const createMessageTag = function (LR_className, senderName, message) {
@@ -197,11 +188,11 @@ window.loadMessageList = function (roomId) {
     const messageQuery = query(
         ref(database, `Messages/${roomId}`),
         // queryConstraints
+        orderByChild('timestamp', 'desc'),
         limitToLast(50)
     );
     onValue(messageQuery, (snapshot) => {
         if (snapshot.exists()) {
-            console.log("Message=", snapshot.val());
             if (roomId) {
                 loadMessage(roomId, snapshot);
             }
@@ -212,7 +203,6 @@ window.loadMessageList = function (roomId) {
         onlyOnce: false
     });
 }
-
 
 
 /**
@@ -242,82 +232,71 @@ const timestampToTime = function (timestamp) {
     return convertDate + convertHour;
 }
 
-function yyyyMMddHHmmsss() {
-    let vDate = new Date(),
-        yyyy = vDate.getFullYear().toString(),
-        MM = (vDate.getMonth() + 1).toString(),
-        dd = vDate.getDate().toString(),
-        HH = vDate.getHours().toString(),
-        mm = vDate.getMinutes().toString(),
-        ss = vDate.getSeconds().toString(),
-        sss = vDate.getMilliseconds().toString();
-
-    return yyyy + (MM[1] ? MM : '0' + MM[0]) + (dd[1] ? dd : '0' + dd[0]) + (HH[1] ? HH : '0' + HH[0])
-        + (mm[1] ? mm : '0' + mm[0]) + (ss[1] ? ss : '0' + ss[0]) + sss;
-}
-
-window.getUserData = function () {
-    get(ref(database), 'Users').then((snapshot) => {
-        if (snapshot.exists()) {
-            console.log(snapshot.val());
-        } else {
-            console.log("유저 데이터 없음");
-        }
-    }).catch((error) => {
-        console.error(error);
-    });
-}
-
 /**
  * 채팅 전송 버튼 클릭 시, firebase Messages 저장, MemberRooms 마지막 메시지 업데이트
  *
- * @param operatorId
- * @param storeName
- * @param userId
- * @param roomId
  * @param message
  */
-window.sendMessage = function (operatorId, storeName, roomId, message) {
+window.sendMessage = function (message) {
 
     /**
-     *  메세지에 태그 입력시 변경하기
+     *  메세지에 태그 입력시 변경하기 (태그 입력 방지)
      */
     const convertMessage = function (html) {
         let tmp = document.createElement("DIV");
         tmp.innerHTML = html;
         return tmp.textContent || tmp.innerText || "";
     }
-    const convertMsg = convertMessage(message);
-    let messageRefKey = ref(database, 'Messages').push().key; // 메세지 키값 구하기
-    let multiUpdates = {};
 
     // 이미 사용자가 메시지를 전송했으므로 메시지를 처음 입력하는 경우 없음 !
     // RoomMembers 업데이트 할 필요 X
+    if (currOperatorId !== 0 && currOperatorId !== undefined && currRoomId !== '') {
+        let multiUpdates = {};
+        const convertMsg = convertMessage(message);
+        // Get a key for a new Post.
+        // const newPostKey = push(child(ref(db), 'posts')).key;
+        const messageRefKey = push(child(ref(database), `Messages/${currRoomId}`)).key; // 메세지 키값 구하기
+        const timestamp = serverTimestamp();  // 서버시간 등록하기
 
-    const timestamp = database.ServerValue.TIMESTAMP  // 서버시간 등록하기
-    // Messages 저장
-    multiUpdates[`Messages/${roomId}/${messageRefKey}`] = {
-        memberUid: `OPERATOR-${operatorId}`,
-        sender: storeName,
-        senderType: "OPERATOR",
-        message: convertMsg, // 태그 입력 방지
-        // profileImg: user.photoURL ? user.photoURL : '',
-        timestamp: timestamp
+        // 네 안녕하세요~ 주문서 작성 부탁드릴게요!
+        console.log("운영자 id=",currOperatorId);  // 없음
+        console.log("채팅방 id=",currRoomId);
+        get(ref(database, `MemberRooms/OPERATOR-${currOperatorId}/${currRoomId}`)).then((snapshot) => {
+            if (snapshot.exists()) {
+                const val = snapshot.val();
+                console.log("채팅방 데이터", val);
+
+                // Messages 저장
+                multiUpdates[`Messages/${currRoomId}/${messageRefKey}`] = {
+                    memberUid: `OPERATOR-${currOperatorId}`,
+                    sender: val.roomOperatorName,
+                    senderType: "OPERATOR",
+                    message: convertMsg,
+                    timestamp: timestamp
+                    // profileImg: user.photoURL ? user.photoURL : '',
+                }
+
+                // MemberRooms 저장
+                multiUpdates[`MemberRooms/OPERATOR-${currOperatorId}/${currRoomId}`] = {
+                    chatStatus: "IN_PROGRESS",  // 진행중
+                    lastMessage: convertMsg, // 태그 입력 방지
+                    roomOperatorId: val.roomOperatorId,
+                    roomOperatorName: val.roomOperatorName,
+                    roomUserId: val.roomUserId,
+                    roomUserName: val.roomUserName,
+                    timestamp: timestamp
+                    // profileImg: user.photoURL ? user.photoURL : '',
+                }
+
+                // Messages, MemberRooms 업데이트
+                update(ref(database), multiUpdates);
+            } else {
+                console.log("채팅방 데이터 없음");
+            }
+        }).catch((error) => {
+            console.error(error);
+        });
     }
 
-    // messagesRef
-    //     .child(`${roomId}`)
-    //     .set({})
-
-    // MemberRooms 저장
-    multiUpdates[`MemberRooms/OPERATOR-${operatorId}/${messageRefKey}`] = {
-        chatStatus: "IN_PROGRESS",  // 진행중
-        lastMessage: convertMsg, // 태그 입력 방지
-        // profileImg: user.photoURL ? user.photoURL : '',
-        timestamp: timestamp
-    }
-
-    // Messages, MemberRooms 업데이트
-    this.database.ref().update(multiUpdates);
 }
 
