4ff79bd83497bcf74d243435feeb8ded8c1a3bf0
ChoiJaeEun <poungki0823@gmail.com>
2023-05-16 23:03:09 +0900

    response에 sheet id 추가

--- custom-cake-backend/src/main/kotlin/com/cake/customcake/adapter/in/web/dto/response/ApproveCustomSheetResponse.kt
+++ custom-cake-backend/src/main/kotlin/com/cake/customcake/adapter/in/web/dto/response/ApproveCustomSheetResponse.kt
@@ -1,5 +1,6 @@
 package com.cake.customcake.adapter.`in`.web.dto.response
 
 data class ApproveCustomSheetResponse(
-    val approve: Boolean
+    val approve: Boolean,
+    val cakeCustomSheetId: Long?
 )
